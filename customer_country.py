e80601187299715237eb536d318869e4bb34c496
Nathan <n8dealey@gmail.com>
2022-09-06 13:05:54 -0500

    Completed Customer CSV code

--- customer_country.py
+++ customer_country.py
@@ -1,5 +1,18 @@
 import csv
 
-infile = open('customers.csv', 'r')
+infile = open("customers.csv", 'r')
+outfile = open("customer_country.csv", 'w')
 
-csvfile = (infile)
+csvfile = csv.reader(infile, delimiter=',')
+
+next(csvfile)
+
+outfile.write('Full Name, Country\n')
+
+for record in csvfile:
+    full_name = record[1] + ' ' + record[2]
+    country = record[4]
+
+    outfile.write(full_name + ',' + country + '\n')
+
+outfile.close
